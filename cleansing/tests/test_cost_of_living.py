7f4b08c5c27864ba2e2a73ea23d1d7c8e33a33c8
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-26 00:08:39 +0900

    fix test

--- cleansing/tests/test_cost_of_living.py
+++ cleansing/tests/test_cost_of_living.py
@@ -3,9 +3,11 @@ from pathlib import Path
 from pprint import pprint
 import json
 from cleansing import cost_of_living
+from collection.cost_of_living import CostOfLivingTask
 
 
 def test_cost_of_living():
+    CostOfLivingTask().run()
     cost_of_living.cleanse()
     output = Path(os.getenv("DATA_DIR")) / "cost_of_living_cleansed.json"
     pprint(json.load(output.open())[:5])
