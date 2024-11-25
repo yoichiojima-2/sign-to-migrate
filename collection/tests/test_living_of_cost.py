cb9d106323ad75b85ab2f7d108894d13d6d45489
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-25 23:54:52 +0900

    update test

--- collection/tests/test_living_of_cost.py
+++ collection/tests/test_living_of_cost.py
@@ -5,8 +5,7 @@ from tests.test_lib import run_and_check_output
 
 
 def test_cost_of_living():
-    output_name = "cost_of_living.json"
-    run_and_check_output(CostOfLivingTask, output_name)
-    df = pd.read_json(f"{os.getenv('DATA_DIR')}/{output_name}")
+    run_and_check_output(CostOfLivingTask, CostOfLivingTask.utput_name)
+    df = pd.read_json(f"{os.getenv('DATA_DIR')}/{CostOfLivingTask.output_name}")
     print(df[["city"]].drop_duplicates().values)
     assert set(CostOfLivingTask.cities) == set(df["city"].drop_duplicates().values)
